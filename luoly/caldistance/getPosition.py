a09692f2cd7652ffbd4ae4fb738d44c33a19bb61
luoluyao <luoluyao520@gmail.com>
2018-01-09 23:56:37 -0800

    calculate position

--- luoly/caldistance/getPosition.py
+++ luoly/caldistance/getPosition.py
@@ -1,9 +1,62 @@
 import sys
 import numpy as np
 from math import sqrt
+from math import isnan
 from sympy import *
+
 # triangle's three edges : 58, d , d
 d = np.sqrt(58 * 58/2)
+
+# data number for phoneme
+phoneme_num = 4
+
+def cal_position_multi(datas):
+    '''
+    Using cal_position to calculate all datas
+    :param datas:
+    :return:
+    '''
+    positions = []
+    tmp = []
+    for data in datas:
+        for d in data:
+            r = cal_position(d[1], d[0], d[2])
+            if r is not None and len(r) != 0:
+                tmp.append(r)
+        positions.append(tmp)
+        tmp = []
+    return positions
+
+def isComplex(num):
+    '''
+    is complex?
+    :return:
+    '''
+    try:
+        float(num)
+        return False
+    except:
+        return True
+
+def calc_z(x, y, r1):
+    '''
+    calcaute the value of z
+    :param x:
+    :param y:
+    :param r1:
+    :param r2:
+    :param r3:
+    :param r4:
+    :return: 0 means wrong!
+    '''
+    z = Symbol('z')
+    z_solve = solve([(x - d) ** 2 + y ** 2 + z ** 2 - r1 ** 2], [z])
+    z_value = z_solve[1][0]  # positive number
+    if not isComplex(z_value):
+      return z_value
+    return 0
+
+
 
 
 def cal_position(r12, r13, r14):
@@ -18,14 +71,18 @@ def cal_position(r12, r13, r14):
     r2 = Symbol('r2')
     r3 = Symbol('r3')
     r4 = Symbol('r4')
-
+    if r14 == 0:
+        print "[error]:wrong input:",r12,r13,r14
+        return
+    #print r12,r13,r14
     result_r =  solve([2*(r12 + r14 - r13)*r1 - (r13**2 - r12**2 - r14**2),
              r1 + r12 - r2,
              r1 + r13 - r3,
-             r1 + r14 - r4
-            ],
+             r1 + r14 - r4],
             [r1,r2,r3,r4])
-
+    if len(result_r) == 0:
+        print "[error]:solve failed"
+        return
     r1 = result_r[r1]
     r2 = result_r[r2]
     r3 = result_r[r3]
@@ -34,88 +91,94 @@ def cal_position(r12, r13, r14):
     x = (r3*r3 - r1*r1) / (4 * d)
     y = (r4*r4 - r2*r2) / (4 * d)
 
-    z = Symbol('z')
-    z_solve = solve([(x - d)**2 + y**2 + z**2 - r1**2],[z])
-    z = z_solve[1][0] # positive number
+    z = calc_z(x,y,r1)
+    if z == 0:
+        print "[error] complex:"
+        return
 
     R = sqrt(x*x + y*y + z*z)
-    return x, y, z, R
+
+    return round(x,2), round(y,2), round(z,2), round(R,2)
 
 def main():
-    r12 = 35.86  # 21
-    r13 = 73.05  # 20
-    r14 = 41.17  # 23
-    result = cal_position(r12, r13, r14)
-    print result
+    file_name = sys.argv[1]
+    file_name_write = sys.argv[2]
+    data = read_file(file_name)
+    datas = transform_data(data)
+    position = cal_position_multi(datas)
+    f_wite = open(file_name_write, "w")
+    for p in position:
+        f_wite.write(str(p))
+        f_wite.write("\n")
+    f_wite.close()
+    print data
+    print datas
+    print position
+
+def transform_data(datas):
+    '''
+    change data to formated data
+    :param data:
+    :return:
+    '''
+    result = []
+    tmp = []
+    for data in datas:
+        for i in range(phoneme_num):
+            tmp.append([d for d in data[i::phoneme_num]])
+        result.append(tmp)
+        tmp = []
+    return result
+
+def cmd_func(a, b):
+    '''
+    compare .wav name
+    :param a:
+    :param b:
+    :return:
+    '''
+    return cmp(a, b)
+
+def read_file(file_name):
+    '''
+    read file datas
+    :param file_name:
+    :return:
+    '''
+    file = open(file_name)
+    test_data = file.readlines()
+    line_count = 1
+    datas = []
+    tmp = []
+    merge_tmp = []
+    for t_data in test_data:
+        t_data = t_data.strip()
+        tmp.append(t_data)
+        if (line_count % 3 == 0):
+            tmp.sort(cmp=cmd_func)
+            tmp = str(tmp).strip()
+            tmp = tmp.replace('[', '')
+            tmp = tmp.replace(']', '')
+            tmp = tmp.replace('"', '')
+            tmp_new = tmp.split(", ")
+            for i in range(len(tmp_new)):
+                if i % (phoneme_num + 2) == 0 or i % (phoneme_num + 2) == 1:
+                    continue
+                merge_tmp.append(abs(float(tmp_new[i])))
+            tmp = []
+            datas.append(merge_tmp)
+            merge_tmp = []
+        line_count += 1
+    file.close()
+    return datas
 
 if __name__ == "__main__":
     main()
 
 
-# r12 = 17.27 #21
-# r13 = 38.52 #20
-# r14 = 15.94 #23
-
-# r12 = -35.86 #21
-# r13 = -71.72 #20
-# r14 = -37.19 #23
-
-# r12 = 17.27 #21
-# r13 = 38.52 #20
-# r14 = 15.94 #23
-#
-# x = Symbol('x')
-# y = Symbol('y')
-# z = Symbol('z')
-# R = Symbol('R')
-# r1 = Symbol('r1')
-# r2 = Symbol('r2')
-# r3 = Symbol('r3')
-# r4 = Symbol('r4')
-#
-#
-# print solve([x**2 + y**2 + z**2 - R**2,
-#              (x - d)**2 + y**2 + z**2 - r1**2,
-#              x**2 + (y - d)**2 + z**2- r2**2,
-#              (x + d)**2 + y**2 + z**2 - r3**2,
-#              x**2 +(y + d)**2 + z**2 - r4**2,
-#              r2 - r1 - r12,
-#              r3 - r1 - r13,
-#              r4 - r1 - r14],
-#             [x, y, z, R, r1, r2, r3, r4])
-
-
-# 
-# 
-# # triangle's three edges : 58, d , d
-# d = np.sqrt(58/2)
-# 
-# # velocity for sound:mm/s
-# v = 340000
-# 
-# # three tdoa:
-# # ATTENTION: signal
-# 
-# #  calc values.
-# 
-# r12 = -17.27 #21
-# r13 = -38.52 #20
-# r14 = -15.94 #23
-# 
-# 
-# r = (np.power(r12,2) + np.power(r14,2) - np.power(r13,2)) / (2 * (r13 - r12 - r14))
-# 
-# angle1 = np.arctan((r14 - r12) / r13)
-# 
-# angle2 = np.arcsin(np.sqrt(np.power(r13, 2) + np.power(r14 - r12, 2))  / (2 * d))
-# 
-# print "r:", r," angle1:", angle1, " angle2:", angle2
-
-
 # ['sound/log/test2.wav', 'sound/log/test3.wav', -15.94, -10.63, -15.94, -18.59]
 # ['sound/log/test2.wav', 'sound/log/test0.wav', -38.52, -42.5, -38.52, -41.17]
 # ['sound/log/test2.wav', 'sound/log/test1.wav', -17.27, -7.97, -17.27, -18.59]
-
 
 # ['sound/log/test2.wav', 'sound/log/test0.wav', -75.7, -87.66, -57.11, -73.05]
 # ['sound/log/test2.wav', 'sound/log/test1.wav', -38.52, -41.17, -38.52, -35.86]
